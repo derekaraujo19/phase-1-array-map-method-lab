38c2440d097b39c81c3f49b00a5cf339ce4f5dc5
Derek Araujo <derekaraujo19@gmail.com>
2021-08-24 17:37:05 -0400

    commit tree 14a819f2746b5a04e6d50d9392b276543f9cc1d5

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+// const titleCased = () => {
+//   let splitWords = tutorials.map(function(sentence) {
+//     let sentenceSplit = sentence.split(' ');
+//     // console.log(sentenceSplit);
+//     let capSentence = [];
+//     for (let i = 0; i < sentenceSplit.length; i++) {
+//       capSentence.push(sentenceSplit[i][0].toUpperCase() + sentenceSplit[i].slice(1));
+//    }
+//     return capSentence.join(' ');
+//   })
+//     return splitWords;
+
+
+//   }
+
 const titleCased = () => {
-  return tutorials
-}
+  let splitWords = tutorials.map(function(sentence) {
+    let sentenceSplit = sentence.split(' ');
+    // console.log(sentenceSplit);
+    let capSentence = sentenceSplit.map(function(word) {
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    return capSentence.join(' ');
+   })
+
+
+    return splitWords;
+
+  }
+
+
+
+
+
+
+
+// itereate through array
+    // mapping a split string on each element (each word will equal element)
+        // map each element so that each element of that is isolated
+            // first element
